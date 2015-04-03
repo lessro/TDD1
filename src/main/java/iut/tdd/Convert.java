2906bb36fe965cb6b5adb472c37a437d7e1729d9
Robin Lefebvre <robin.lefebvre.59@etudiant.univ-lille1.fr>
2015-04-03 16:29:19 +0200

    bonjour_le_test

--- src/main/java/iut/tdd/Convert.java
+++ src/main/java/iut/tdd/Convert.java
@@ -1,7 +1,21 @@
 package iut.tdd;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Convert {
+	static String [] chiffre ={"0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","20"};
+	static String [] chiffreenlettre ={"zéro","un","deux","trois","quatre","cinq","six","sept","huit","neuf","dix","onze","douze","treize","quatorze","quinze","seize","vingt"};
+	
+		
+	
 	public static String num2text(String input) {
+		for (int i = 0 ; i<chiffre.length;i++){
+			if (chiffre[i].equals(input)){
+				return chiffreenlettre[i];
+			}
+		}
+		
 		return null;
 	}
 	public static String text2num(String input) {
